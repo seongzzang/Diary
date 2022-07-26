6030168042405591ab1c15f85e56f01c8709815a
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-07-26 21:44:26 +0900

    diary_writediaryview_update

---
+++ Diary/WrtieDiaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WrtieDiaryViewController.swift
+//  Diary
+//
+//  Created by 양성혜 on 2022/07/26.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
