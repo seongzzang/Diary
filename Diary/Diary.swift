aace3451a286cb2d9425a0169c2511fa3f42b95f
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-07-26 22:50:05 +0900

    diary_confrim&saveData

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 양성혜 on 2022/07/26.
+//
+
+import Foundation
+
+struct Diary{
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
